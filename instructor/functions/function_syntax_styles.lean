501f6556af04a5fa43957ec4b54b88f52a738474
Kevin Sullivan <sullivan@virginia.edu>
2020-01-31 17:29:49 -0500

    Minor edits

--- instructor/functions/function_syntax_styles.lean
+++ instructor/functions/function_syntax_styles.lean
@@ -84,13 +84,16 @@ def ite'' : bool → ℕ → ℕ → ℕ :=
         end
 
 
+-- Quick preview: higher-order functions
 
 def inc (n : ℕ) := n + 1
 def sq (n : ℕ) := n^2
 
---def is := inc ∘ sq
---#eval is 7
+def is := inc ∘ sq  -- function composition
+#eval is 7
 
+-- How to implement it yourself
+-- Takes two functions and returns composition
 def comp (f g : ℕ → ℕ) :=
     λ (n : ℕ), 
         f (g n)
