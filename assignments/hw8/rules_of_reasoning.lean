72c2e76741650d0bf126aedc26a71516af8bb0c5
Kevin Sullivan <sullivan.kevinj@gmail.com>
2020-03-29 10:40:58 -0400

    .

--- assignments/hw8/rules_of_reasoning.lean
+++ assignments/hw8/rules_of_reasoning.lean
@@ -27,20 +27,46 @@ scenario that shows that the formula is
 not always true. 
 
 To do this assignment, produce a truth table
-for each formula. Start by defining all of
-the possible interpretations for the three
-propositional variables we've defined. You
-can call them i_0, i_1, ... etc. Beneath
-each formula, evaluate it under each of the
-possible interpretations. The results will
-tell you whether the formula is valid or not.
-It'll also tell you under which interpretation
-if any a formula is not true.
+for each formula and use the result to tell 
+if a formula is valid or not. Remember that
+each row of a truth table corresponds to one
+interpretation and gives the value of such a
+formula under that specific interpretation. 
+
+Some of the formulae contain only one variable.
+We will always use P in these cases. You will
+need two interpretations in these cases. Call
+them Pt and Pf. Some formula have two variables,
+P and Q. You will need four interpretations in
+these cases. Call them PtQt, PtQf, etc. Finally
+some formula use three variables. Call the
+interpretations for these cases PtQtRt, etc.
+
+Define your interpretations, for formulae with
+one, two, and three variables, respectively, here. 
 -/
 
+-- Answer
+
+
+/-
+Beneath each formula, below, evaluate it under 
+each of its possible interpretations. Hint: use
+pEval!
+
+The results will tell you whether the formula
+is valid or  not. It'll also tell you under 
+which of the interpretations, if any, it is
+not true.
+
+From the results, you can then decide whether
+each formula is valid or not. 
+-/
 
 def true_intro : pExp := pTrue
--- answer here
+
+-- truth table here (some number of pEvals)
+-- classification here (valid or not)
 
 def false_elim := pFalse > P
 -- answer here
@@ -49,23 +75,56 @@ def true_imp := pTrue > P
 -- etc
 
 def and_intro := P > Q > P ∧ Q
+
 def and_elim_left := P ∧ Q > P
+
 def and_elim_right := P ∧ Q > Q
+
 def or_intro_left := P > (P ∨ Q)
+
 def or_intro_right := Q > (P ∨ Q)
+
 def or_elim := (P ∨ Q) > (P > R) > (Q > R) > R
+
 def iff_intro :=   (P > Q) > (Q > P) > (P ↔ Q)
+
 def iff_intro' := ((P > Q) ∧ (Q > P)) > (P ↔ Q)
+
 def iff_elim_left := (P ↔ Q) > (P > Q)
+
 def iff_elim_right := (P ↔ Q) > (Q > P)
+
 def arrow_elim := (P > Q) > P > Q
+
 def resolution := (P ∨ Q) > (¬ Q ∨ R) > (P ∨ R)
+
 def unit_resolution := (P ∨ Q) > (¬ Q) > P
+
 def syllogism := (P > Q) > (Q > R) > (P > R)
+
 def modus_tollens := (P > Q) > ¬ Q > ¬ P
+
 def neg_elim := ¬ ¬ P > P
+
 def excluded_middle := P ∨ ¬ P
+
 def neg_intro := (P > pFalse) > ¬ P
+
 def affirm_consequence := (P > Q) > Q > P
+
 def affirm_disjunct := (P ∨ Q) > P > ¬ Q
+
 def deny_antecedent := (P > Q) > (¬ P > ¬ Q)
+
+
+/-
+Study the valid rules and learn their names. 
+These rules, which, here, we are validating 
+"semantically" (by the method of truth tables)
+will become fundamental "rules of inference",
+for reasoning "syntactically" when we get to 
+predicate logic. There is not much memorizing
+in this class, but this is one case where you
+will find it important to learn the names and
+definitions of these rules.
+-/
